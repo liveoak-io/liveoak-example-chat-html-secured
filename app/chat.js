885d78aca40e76a325a7953f38d872f4ac151761
Ken Finnigan <ken@kenfinnigan.me>
2015-01-28 16:11:53 -0500

    LIVEOAK-723 Update secured chat example to use new styling

--- app/chat.js
+++ app/chat.js
@@ -16,25 +16,31 @@ $( function() {
   });
 
   function add_message(data) {
-    $( '#messages' ).append( 
-      $( '<div class="message" id="' + get_id( data ) + '">' ).append(
-        $('<div class="name">').append( data.name ) ).append( 
-        $('<div class="name">').append( data.text ) ).append(
-        $('<div class="text">').append(
-          $('<button id="delete_' + get_id( data ) + '">' ).click(function() {
-            trigger_remove_message(this.id);
-          }).append ("Delete message") ) ) );
-    $( '#messages' ).scrollTop( $('#messages')[0].scrollHeight );
+    $( '#welcome').hide();
+    $( '#messages' ).append(
+      $( '<div class="message clearfix" id="' + get_id( data ) + '">' ).append(
+        $('<span class="badge badge-primary pull-left" style="background-color:' + get_color(data.name) + '">').append( data.name[0] ) ).append(
+        $('<div class="name-text pull-left">').append(
+          $('<div class="name">').append( data.name ) ).append(
+          $('<div class="text">').append( data.text ) ).append(
+          $('<div class="text">').append(
+            $('<button id="delete_' + get_id( data ) + '">' ).click(function() {
+                trigger_remove_message(this.id);
+              }).append ("Delete message") ) ) ) );
+    window.scrollTo(0, document.body.scrollHeight);
   }
 
   function remove_message(data) {
     $( '#' + get_id( data ) ).remove();
+    if ($( '.message' ).length === 0) {
+      $( '#welcome').show();
+    }
   }
 
   function update_message(data) {
     var msg = $( '#' + get_id( data ) );
-    msg.find( $('.name') )[0].textContent = data.name ;
-    msg.find( $('.name') )[1].textContent = data.text ;
+    msg.find( '.name' ).html( data.name );
+    msg.find( '.text' ).html( data.text );
   }
 
   function trigger_remove_message(msgId) {
@@ -55,9 +61,40 @@ $( function() {
     return msgId;
   }
 
+  function hash(str) {
+    var hash = 0, i, chr, len;
+    if (str.length == 0) return hash;
+    for (i = 0, len = str.length; i < len; i++) {
+      chr   = str.charCodeAt(i);
+      hash  = ((hash << 5) - hash) + chr;
+      hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+  }
+
+  function get_color(name) {
+    if (usedColors[name]) {
+      return usedColors[name];
+    }
+    else {
+      if (colors.length === 0) {
+        colors = colorsBackup.slice(0);
+      }
+      var idx = Math.abs(hash(name)) % colors.length;
+      var color = colors[idx]
+      usedColors[name] = color;
+      colors.splice(idx, 1);
+      return color;
+    }
+  }
+
+  var colors = ['#F44336','#E91E63','#9C27B0','#673AB7','#3F51B5','#2196F3','#03A9F4','#00BCD4','#009688','#4CAF50','#8BC34A','#CDDC39','#FFEB3B','#FFC107','#FF9800','#FF5722','#795548','#9E9E9E','#607D8B','#000000'];
+  var colorsBackup = colors.slice(0);
+  var usedColors = {};
+
   function authCallback() {
     $('body').css('display', 'block');
-    $( '#user-info' ).html( "Logged in as: " + liveoak.auth.idToken.preferred_username );
+    $( '.namebar' ).html( '<span id="name-field">Logged in as: ' + liveoak.auth.idToken.preferred_username + '</span>' );
     liveoak.connect( "Bearer", liveoak.auth.token, function() {
       // If admin is logged, we will try to create chat collection
       if (liveoak.auth.hasResourceRole('admin', 'chat-html-secured')) {
@@ -102,16 +139,25 @@ $( function() {
     liveoak.auth.logout();
   });
 
+  $('#input').keypress(function(e) {
+    if (e.which == 13) {
+      $('#input form').submit();
+      return false;
+    }
+  });
+
   $('#input form').submit( function() {
     var name = liveoak.auth.idToken.preferred_username;
     var text = $( '#text-field' ).val();
+
+    if (!name || !text) return false;
 
     $('#text-field').val( '' );
 
     liveoak.create( '/chat-html-secured/storage/chat',
                     { name: name, text: text },
-                    { success: function(data) { 
-                        console.log( "sent" ); 
+                    { success: function(data) {
+                        console.log( "sent" );
                       },
                       error: function(error) {
                         alert( error.status + ": " + error.statusText );
